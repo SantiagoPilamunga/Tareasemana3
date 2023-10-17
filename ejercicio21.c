b78403a55ba7f2a6e05debf3661b1cefeae379cf
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 00:40:03 -0500

    setencia while

--- ejercicio21.c
+++ ejercicio21.c
@@ -3,9 +3,18 @@
 int main()
 {
     int num;
+    int suma=0;
     printf("Ingrese un numero entero \n");
     scanf("%d",&num);
-    num=trunc(num/10);
+    while (num>=1)
+    {
+        suma+=num%10;
+        num=trunc(num/10);
+    }
+    printf("SSDSAS %d",suma);
+    
+    
+    
     return 0;
     
 }
