a3ac69645cd65ec8516f571ee2b32c915af82d0b
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 00:55:05 -0500

    cambios finales

--- ejercicio21.c
+++ ejercicio21.c
@@ -4,14 +4,16 @@ int main()
 {
     int num;
     int suma=0;
+    int num2=0;
     printf("Ingrese un numero entero \n");
     scanf("%d",&num);
+    num2+=num;
     while (num>=1)
     {
         suma+=num%10;
         num=trunc(num/10);
     }
-    printf("La suma de los digitos del numero es: %d",suma);
+    printf("La suma de los digitos del numero %d es: %d \n",num2,suma);
     return 0;
     
 }
