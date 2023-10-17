8e6299d2ef0fbd21a0de567a36a66f893803db7e
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-16 22:34:03 -0500

    resultado promedio

--- ejercicio31.c
+++ ejercicio31.c
@@ -4,20 +4,21 @@ int main()
     int num=1;
     int acumulador=0;
     int contador=0;
+    int promedio;
     
     while (num!=0)
     {
         printf("Ingrese un numero: \n");
-        scanf("%d",&num);
+        scanf("%d\n",&num);
         if (num%10==5)
         {
             acumulador+=num;
             contador+=1;
             
         }
-        
+        printf("Para finalizar el programa digite 0 \n");
         
     }
-    
+    promedio=acumulador/contador;
     return 0;
 }
