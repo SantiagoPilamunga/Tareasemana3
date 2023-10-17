efddcdbabac075b2cf343ca3a02b9f207e53a1c0
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 01:25:22 -0500

    separamos los digitos

--- ejercicio11.c
+++ ejercicio11.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     int num;
+    int num1;
+    int num2;
     printf("Ingrese un numero entero de dos digitos \n");
-    scanf("%d",num);
+    scanf("%d",&num);
     if (num>0 && num<=99)
     {
-        
+
+        num1=num%10;
+        num2=trunc(num/10);
+        printf("%d %d",num1,num2);
+
     }else{
         printf("El ejercicio no se puede realizar");
     }
