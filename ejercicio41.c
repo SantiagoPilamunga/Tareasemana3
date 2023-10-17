b460e4b8c325ea482de0765c065ddc99617b659d
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-16 20:40:15 -0500

    condicional para no pasar de 100

--- ejercicio41.c
+++ ejercicio41.c
@@ -4,12 +4,17 @@ int main()
     int num1=0;
     int num2=1;
     int suma=0;
-    while (suma<100)
+    int sumatotal=0;
+    while (suma<=100)
     {
         suma=num1+num2;
         num1=num2;
         num2=suma;
-        
+        if (num2<100)
+        {
+            sumatotal+=suma;
+        }
     }
+    
     return 0;
 }
