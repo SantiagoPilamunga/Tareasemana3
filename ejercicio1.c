010916908d591b727ecc3513593b484f2b284bee
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 02:01:55 -0500

    correcciones

--- ejercicio1.c
+++ ejercicio1.c
@@ -5,6 +5,7 @@ int main()
     int i;
     printf("Ingrese un numero entero");
     scanf("%d",&num);
+    printf("Los enteros entre 1 y %d son: ",num);
     for ( i = 1; i < num; i++)
     {
         printf(" %d ",i);
