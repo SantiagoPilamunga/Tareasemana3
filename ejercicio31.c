aa23776366617087a42a6cbb597841d97b2a62d2
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-16 22:30:48 -0500

    correccion

--- ejercicio31.c
+++ ejercicio31.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int num;
+    int num=1;
     int acumulador=0;
     int contador=0;
-    scanf("%d",&num);
+    
     while (num!=0)
     {
-        if (num%5==0)
+        printf("Ingrese un numero: \n");
+        scanf("%d",&num);
+        if (num%10==5)
         {
             acumulador+=num;
             contador+=1;
