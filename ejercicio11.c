ed282b10453336f80a090a303edd81b971f24e53
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 01:17:30 -0500

    añadimos un if

--- ejercicio11.c
+++ ejercicio11.c
@@ -4,5 +4,12 @@ int main()
     int num;
     printf("Ingrese un numero entero de dos digitos \n");
     scanf("%d",num);
+    if (num>0 && num<=99)
+    {
+        
+    }else{
+        printf("El ejercicio no se puede realizar");
+    }
+    
     return 0;
 }
