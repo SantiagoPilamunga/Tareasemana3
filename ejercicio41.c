4853bb60c517f254468ff4341fc5f1fa6b5fffd8
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-16 19:02:20 -0500

    repetitiva

--- ejercicio41.c
+++ ejercicio41.c
@@ -4,7 +4,12 @@ int main()
     int num1=0;
     int num2=1;
     int suma=0;
-   
-    
+    while (suma<100)
+    {
+        suma=num1+num2;
+        num1=num2;
+        num2=suma;
+        
+    }
     return 0;
 }
