e68310bc778e2fad4cb3a127f836c409d4d812c1
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-16 22:38:58 -0500

    correcciones finales

--- ejercicio31.c
+++ ejercicio31.c
@@ -9,16 +9,16 @@ int main()
     while (num!=0)
     {
         printf("Ingrese un numero: \n");
-        scanf("%d\n",&num);
+        scanf("%d",&num);
+        printf("Para finalizar el programa digite 0 \n");
         if (num%10==5)
         {
             acumulador+=num;
             contador+=1;
             
         }
-        printf("Para finalizar el programa digite 0 \n");
-        
     }
     promedio=acumulador/contador;
+    printf("El promedio de los numeros terminados en 5 es: %d  \n",promedio);
     return 0;
 }
