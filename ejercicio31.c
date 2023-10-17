134778077daf2a521d8531eb8d71921adaf0a1cc
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-16 22:20:46 -0500

    agregamos sentencia while e if

--- ejercicio31.c
+++ ejercicio31.c
@@ -2,9 +2,18 @@
 int main()
 {
     int num;
+    int acumulador=0;
+    int contador=0;
     scanf("%d",&num);
     while (num!=0)
     {
+        if (num%5==0)
+        {
+            acumulador+=num;
+            contador+=1;
+            
+        }
+        
         
     }
     
