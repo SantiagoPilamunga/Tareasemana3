9eb2686f794f81461ee5ab1b9dc727815975b171
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 00:32:58 -0500

    añadimos otra libreria

---
+++ ejercicio21.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num;
+    printf("Ingrese un numero entero \n");
+    scanf("%d",&num);
+    num=trunc(num/10);
+    return 0;
+    
+}
