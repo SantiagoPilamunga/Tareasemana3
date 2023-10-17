07c12647b2b6024028994a0ab40e497479da22ef
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-17 01:58:38 -0500

    utilizamos for

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,7 +2,13 @@
 int main()
 {
     int num;
+    int i;
     printf("Ingrese un numero entero");
     scanf("%d",&num);
+    for ( i = 1; i < num; i++)
+    {
+        printf("%d",i);
+    }
+    
     return 0;
 }
